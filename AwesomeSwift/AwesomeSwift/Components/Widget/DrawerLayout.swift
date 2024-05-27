3d07b83fd2976a624fc807cbaa0bd722b1f0609e
Shouheng88 <shouheng2015@gmail.com>
2024-05-27 19:59:26 +0800

    feat: add drawer gesture

--- AwesomeSwift/AwesomeSwift/Components/Widget/DrawerLayout.swift
+++ AwesomeSwift/AwesomeSwift/Components/Widget/DrawerLayout.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 /// 抽屉控件
 struct DrawerLayout<MainContent: View, DrawerContent: View>: View {
-    
+        
     /// 主界面的内容
     let mainContent: MainContent
     
@@ -26,11 +26,51 @@ struct DrawerLayout<MainContent: View, DrawerContent: View>: View {
     /// 抽屉的背景颜色
     var drawerBackground: Color = .red
     
+    /// 抽屉展开时向右滑动的判断阈值
+    var drawerDragOpenStartThreshold: CGFloat = 150
+
+    /// 抽屉展开时向右滑动的距离的判断阈值
+    var drawerDragOpenLengthThreshold: CGFloat = 100
+    
+    /// 抽屉关闭时向左滑动的距离的判断阈值
+    var drawerDragCloseThreshold: CGFloat = 100
+    
     @Binding var isDrawerOpen: Bool
+        
+    @State private var drawerOffset = CGSize.zero
+
+    @State private var mainStartLocation: CGPoint? = nil
     
     var body: some View {
         ZStack {
-            // 底部阴影
+            // 主内容区域
+            mainContent
+                .zIndex(1)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ gesture in
+                            let touchLocation = gesture.location
+                            if mainStartLocation == nil {
+                                mainStartLocation = touchLocation
+                            }
+                        })
+                        .onEnded({ value in
+//                            print("底部拖拽：\(String(describing: mainStartLocation?.x)) \(value.location.x)")
+                            if mainStartLocation != nil && mainStartLocation!.x < self.drawerDragOpenStartThreshold {
+                                let draggedLength = value.location.x - mainStartLocation!.x
+                                let isDrawerOpenTrigged = draggedLength > self.drawerDragOpenLengthThreshold
+//                                print("检测到左侧 100 滑动事件：\(draggedLength)")
+                                if isDrawerOpenTrigged {
+//                                    print("触发了抽屉展开事件")
+                                    withAnimation {
+                                        self.isDrawerOpen = true
+                                    }
+                                }
+                            }
+                            mainStartLocation = nil
+                        })
+                )
+            // 阴影遮罩
             blackShadeColor
                 .ignoresSafeArea(.all)
                 .animation(.easeInOut(duration: 1))
@@ -40,8 +80,7 @@ struct DrawerLayout<MainContent: View, DrawerContent: View>: View {
                         self.isDrawerOpen = false
                     }
                 }
-            // 主内容区域
-            mainContent.zIndex(1)
+                .zIndex(1)
             // 抽屉式菜单
             if isDrawerOpen {
                 HStack(spacing: 0) {
@@ -50,8 +89,27 @@ struct DrawerLayout<MainContent: View, DrawerContent: View>: View {
                         .background(Rectangle().fill(drawerBackground))
                     Spacer()
                 }.transition(.move(edge: .leading))
-                    .edgesIgnoringSafeArea(.vertical)
+                    // 抽屉的区域控制，根据需要自定义吧
+//                    .edgesIgnoringSafeArea(.vertical)
                     .zIndex(2)
+                    .offset(x: min(0, self.drawerOffset.width))
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                self.drawerOffset = value.translation
+                            }
+                            .onEnded { value in
+                                // 拖动结束后重置偏移量
+                                if value.translation.width > -self.drawerDragCloseThreshold {
+                                    self.drawerOffset = .zero
+                                } else {
+                                    withAnimation {
+                                        self.isDrawerOpen = false
+                                        self.drawerOffset = .zero
+                                    }
+                                }
+                            }
+                    )
             }
         }
     }
@@ -65,12 +123,17 @@ struct DrawerLayoutSampleView: View {
     var body: some View {
         DrawerLayout(
             mainContent: VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                }
                 Text("Main Content").padding()
                 Button("Open Drawer") {
                     withAnimation {
                         self.isDrawerOpen.toggle()
                     }
                 }.foregroundColor(.white)
+                Spacer()
             }.background(Rectangle().fill(.blue)),
             drawerContent: VStack {
                 Spacer()
