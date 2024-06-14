f308f37dc8e14dd991114df51fd0c425257ebc7f
Shouheng88 <shouheng2015@gmail.com>
2024-06-14 17:28:49 +0800

    custom dialog demo

---
+++ AwesomeSwift/AwesomeSwift/Components/Widget/Dialog/CustomDialogV2.swift
@@ -0,0 +1,8 @@
+//
+//  CustomDialogV2.swift
+//  AwesomeSwift
+//
+//  Created by shouh wang on 2024/6/13.
+//
+
+import Foundation
---
+++ AwesomeSwift/AwesomeSwift/Components/Widget/DialogDemoView.swift
@@ -0,0 +1,8 @@
+//
+//  DialogDemoView.swift
+//  AwesomeSwift
+//
+//  Created by shouh wang on 2024/6/13.
+//
+
+import Foundation
